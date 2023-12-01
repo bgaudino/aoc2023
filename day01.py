b258ed8d6b6fc7e5c70287a154264e6932e827b2
bgaudino <bgaudino@gmail.com>
2023-12-01 07:44:31 -0600

    Day 1

---
+++ day01.py
@@ -0,0 +1,35 @@
+import re
+
+NUMBERS = {
+    'zero': '0',
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+
+
+def main():
+    with open('data/day01.txt') as f:
+        document = f.readlines()
+
+    part1 = sum(get_calibration_value(line) for line in document)
+    print(f'Part 1: {part1}')
+
+    pattern = fr"(?=(\d|{'|'.join(NUMBERS)}))"
+    part2 = sum(get_calibration_value(line, pattern) for line in document)
+    print(f'Part 2: {part2}')
+
+
+def get_calibration_value(text, pattern=r'\d'):
+    matches = re.findall(pattern, text)
+    return int(''.join(m if m.isdigit() else NUMBERS[m] for m in (matches[0], matches[-1])))
+
+
+if __name__ == '__main__':
+    main()
