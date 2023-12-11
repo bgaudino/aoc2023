09dd0e1c5cd233b93bd25a5f6609f3a18e34a2b0
bgaudino <bgaudino@gmail.com>
2023-12-11 10:59:30 -0600

    Day 11 part 2

--- day11.py
+++ day11.py
@@ -6,46 +6,55 @@ from itertools import combinations
 class Image:
     map: list[str]
 
-    def expand(self):
-        rows: set[int] = set()
-        columns: set[int] = set()
+    def empty_space(self) -> tuple[list[int], list[int]]:
+        rows: list[int] = []
+        columns: list[int] = []
         for y, row in enumerate(self.map):
             for x, char in enumerate(row):
                 if char == '#':
-                    rows.add(y)
-                    columns.add(x)
+                    rows.append(y)
+                    columns.append(x)
+        rows = [i for i in range(len(self.map)) if i not in rows]
+        columns = [i for i in range(len(self.map[0])) if i not in columns]
+        return rows, columns
 
-        empty_rows = {i for i in range(len(self.map)) if i not in rows}
-        empty_columns = {i for i in range(
-            len(self.map[0])) if i not in columns}
-        offset = 0
-        for i in sorted(empty_rows):
-            self.map.insert(i + offset, '.' * len(self.map[0]))
-            offset += 1
-        offset = 0
-        for i in sorted(empty_columns):
-            for y, row in enumerate(self.map):
-                self.map[y] = f'{row[:i + offset]}.{row[i + offset:]}'
-            offset += 1
-
-    def galaxies(self) -> set[tuple[int, int]]:
-        galaxies: set[tuple[int, int]] = set()
+    def galaxies(self) -> list[tuple[int, int]]:
+        galaxies: list[tuple[int, int]] = list()
         for y, row in enumerate(self.map):
             for x, char in enumerate(row):
                 if char == '#':
-                    galaxies.add((x, y))
+                    galaxies.append((x, y))
         return galaxies
 
+    def expanded(self, rate: int) -> list[tuple[int, int]]:
+        galaxies: list[tuple[int, int]] = list()
+        rows, columns = self.empty_space()
+        for x, y in self.galaxies():
+            x += len([column for column in columns if column < x]) * rate
+            y += len([row for row in rows if row < y]) * rate
+            galaxies.append((x, y))
+        return galaxies
 
-with open('data/day11.txt') as f:
-    data = [line.strip() for line in f.readlines()]
+    def total_distance(self, expansion_rate: int = 2) -> int:
+        return sum(distance(a, b) for a, b in combinations(self.expanded(expansion_rate - 1), 2))
 
 
 def distance(a: tuple[int, int], b: tuple[int, int]) -> int:
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 
-image = Image(data)
-image.expand()
-part1 = sum(distance(a, b) for a, b in combinations(image.galaxies(), 2))
-print(part1)
+def main():
+    with open('data/day11.txt') as f:
+        image = Image([line.strip() for line in f.readlines()])
+
+    part1 = image.total_distance()
+    assert part1 == 9734203
+    print(f'Part 1: {part1}')
+
+    part2 = image.total_distance(1_000_000)
+    assert part2 == 568914596391
+    print(f'Part 2: {part2}')
+
+
+if __name__ == '__main__':
+    main()
