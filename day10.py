0205a80a65586473fe9dba2369727a7e802bb2b7
bgaudino <bgaudino@gmail.com>
2023-12-10 22:41:58 -0600

    Day 10 part 2

--- day10.py
+++ day10.py
@@ -26,13 +26,15 @@ PIPES = {
 pipes: dict[Coord, tuple[Coord, Coord]] = {}
 start = None
 with open('data/day10.txt') as f:
-    for y, line in enumerate(f):
-        for x, c in enumerate(line.strip()):
-            if c == 'S':
-                start = (x, y)
-            pipe = PIPES.get(c)
-            if pipe is not None:
-                pipes[(x, y)] = pipe
+    maze = [line.strip() for line in f]
+
+for y, line in enumerate(maze):
+    for x, c in enumerate(line):
+        if c == 'S':
+            start = (x, y)
+        pipe = PIPES.get(c)
+        if pipe is not None:
+            pipes[(x, y)] = pipe
 
 
 def find_loop(start: Coord, pipes: dict[Coord, tuple[Coord, Coord]]) -> list[tuple[Coord, Coord]]:
@@ -55,6 +57,33 @@ def move(pipe: Coord, direction: Coord) -> Coord:
     return (pipe[0] + direction[0], pipe[1] + direction[1])
 
 
-if start is not None:
-    loop = find_loop(start, pipes)
-    print(len(loop) // 2)
+if start is None:
+    exit()
+
+loop = find_loop(start, pipes)
+print(len(loop) // 2)
+
+loop_pipes = {pipe for pipe, _ in loop}
+
+inside_count = 0
+for y, row in enumerate(maze):
+    groups: list[list[Coord]] = []
+    group: list[Coord] = []
+    seperator: str = ''
+    for x, char in enumerate(row):
+        if (x, y) not in loop_pipes:
+            group.append((x, y))
+            seperator = ''
+        elif char in ('F', 'L'):
+            seperator = char
+        elif char == '|' or (seperator == 'F' and char == 'J') or (seperator == 'L' and char == '7'):
+            groups.append(group)
+            group = []
+            seperator = ''
+        elif char == '-' and seperator in ('F', 'L'):
+            continue
+        else:
+            seperator = ''
+    count = sum(len(group) for i, group in enumerate(groups) if i % 2 != 0)
+    inside_count += count
+print(inside_count)
