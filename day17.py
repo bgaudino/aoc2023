3e973264093e52ce400ed164cc54433fcc4f8909
bgaudino <bgaudino@gmail.com>
2023-12-18 10:59:34 -0600

    Day 17 part 2

--- day17.py
+++ day17.py
@@ -1,51 +1,84 @@
 import math
 import heapq
+from typing import NamedTuple
+
 
 with open('data/day17.txt') as f:
-    city = [[int(n) for n in row.strip()] for row in f]
+    CITY = [[int(n) for n in row.strip()] for row in f]
 
-max_x = len(city[0])
-max_y = len(city)
+MAX_X = len(CITY[0])
+MAX_Y = len(CITY)
 
-def solve(part2: bool = False):
-    queue = [(0, 0, 0, -1, -1)]
-    seen = set() 
-    min_heat = math.inf
+
+class Position(NamedTuple):
+    x: int
+    y: int
+
+
+class Step(NamedTuple):
+    position: Position
+    direction: int
+    moves: int
+
+
+DIRECTIONS = (Position(-1, 0), Position(0, 1), Position(1, 0), Position(0, -1))
+GOAL = Position(MAX_X - 1, MAX_Y - 1)
+
+
+def move(p1: Position, p2: Position) -> Position:
+    return Position(p1.x + p2.x, p1.y + p2.y)
+
+
+def in_bounds(p: Position):
+    return 0 <= p.x < MAX_X and 0 <= p.y < MAX_Y
+
+
+def minimum_heat_loss(ultra_crucibles: bool = False):
+    queue: list[tuple[int, Step]] = [(0, Step(Position(0, 0), -1, -1))]
+    seen: set[Step] = set()
+    min_heat_loss = math.inf
     while queue:
-        dist, r, c, dir_, indir = heapq.heappop(queue)
-        if (r, c, dir_, indir) in seen:
+        heat_loss, step = heapq.heappop(queue)
+        if step in seen:
             continue
-        seen.add((r, c, dir_, indir))
-        if (c, r) == (max_x - 1, max_y - 1):
-            min_heat = min(min_heat, dist)
+        if step.position == GOAL:
+            min_heat_loss = min(min_heat_loss, heat_loss)
             continue
-        for i, (dr, dc) in enumerate([[-1, 0], [0, 1], [1, 0], [0, -1]]):
-            rr = r+dr
-            cc = c+dc
-            new_dir = i
-            new_indir = (1 if new_dir != dir_ else indir+1)
+        seen.add(step)
 
-            isnt_reverse = ((new_dir + 2) % 4 != dir_)
-
-            isvalid = (new_indir <= 3)
-            # isvalid_part2 = (new_indir <= 10 and (
-            #     new_dir == dir_ or indir >= 4 or indir == -1))
-            # isvalid = (isvalid_part2 if part2 else isvalid_part1)
-
-            if 0 <= rr < max_y and 0 <= cc < max_y and isnt_reverse and isvalid:
-                cost = city[rr][cc]
-                if (rr, cc, new_dir, new_indir) in seen:
+        for new_direction, delta in enumerate(DIRECTIONS):
+            if (new_direction + 2) % 4 == step.direction:
+                continue
+            new_moves = step.moves + 1 if new_direction == step.direction else 1
+            if ultra_crucibles:
+                if new_moves > 10:
                     continue
-                heapq.heappush(queue, (dist+cost, rr, cc, new_dir, new_indir))
+                if 0 < step.moves < 4 and new_direction != step.direction:
+                    continue
+            elif new_direction == step.direction:
+                if new_moves > 3:
+                    continue
+            new_position = move(step.position, delta)
+            if not in_bounds(new_position):
+                continue
 
-    return min_heat
-    ans = 1e9
-    for (r, c, dir_, indir), v in D.items():
-        if r == max_y-1 and c == max_y-1 and (indir >= 4 or not part2):
-            ans = min(ans, v)
-    return ans
+            new_heat_loss = heat_loss + CITY[new_position.y][new_position.x]
+            next_step = Step(new_position, new_direction, new_moves)
+            if next_step in seen:
+                continue
+            heapq.heappush(queue, (new_heat_loss, next_step))
+
+    return min_heat_loss
 
 
-part1 = solve()
-print(part1)
-assert part1 == 953
+def main():
+    part1 = minimum_heat_loss()
+    print(part1)
+    assert part1 == 953
+    part2 = minimum_heat_loss(ultra_crucibles=True)
+    print(part2)
+    assert part2 == 1180
+
+
+if __name__ == '__main__':
+    main()
